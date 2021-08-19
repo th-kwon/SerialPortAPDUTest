209264f09f0cc4660e8878b225cb38ab8d272aeb
th-kwon <th.kwon29@gmail.com>
2021-08-19 10:16:06 +0900

    Initial Commit

---
+++ SeriaPortAPDUTest/Form1.Designer.cs
@@ -0,0 +1,318 @@
+﻿namespace SerialAPDUTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.comboBoxBaudRate = new System.Windows.Forms.ComboBox();
+            this.comboBoxParity = new System.Windows.Forms.ComboBox();
+            this.comboBoxStopBits = new System.Windows.Forms.ComboBox();
+            this.comboBoxHandShake = new System.Windows.Forms.ComboBox();
+            this.labelDebug = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.textBoxDataBits = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxReadTimeout = new System.Windows.Forms.TextBox();
+            this.textBoxWriteTimeout = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(30, 247);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(167, 52);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Enabled = false;
+            this.buttonClose.Location = new System.Drawing.Point(203, 247);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(167, 53);
+            this.buttonClose.TabIndex = 1;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Baud Rate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Parity";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 132);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 15);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Data Bits";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Stop Bits";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 201);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "HandShake";
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Location = new System.Drawing.Point(124, 22);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(144, 23);
+            this.comboBoxPort.TabIndex = 8;
+            // 
+            // comboBoxBaudRate
+            // 
+            this.comboBoxBaudRate.FormattingEnabled = true;
+            this.comboBoxBaudRate.Location = new System.Drawing.Point(124, 58);
+            this.comboBoxBaudRate.Name = "comboBoxBaudRate";
+            this.comboBoxBaudRate.Size = new System.Drawing.Size(144, 23);
+            this.comboBoxBaudRate.TabIndex = 9;
+            // 
+            // comboBoxParity
+            // 
+            this.comboBoxParity.FormattingEnabled = true;
+            this.comboBoxParity.Location = new System.Drawing.Point(124, 92);
+            this.comboBoxParity.Name = "comboBoxParity";
+            this.comboBoxParity.Size = new System.Drawing.Size(144, 23);
+            this.comboBoxParity.TabIndex = 10;
+            // 
+            // comboBoxStopBits
+            // 
+            this.comboBoxStopBits.FormattingEnabled = true;
+            this.comboBoxStopBits.Location = new System.Drawing.Point(124, 165);
+            this.comboBoxStopBits.Name = "comboBoxStopBits";
+            this.comboBoxStopBits.Size = new System.Drawing.Size(144, 23);
+            this.comboBoxStopBits.TabIndex = 12;
+            // 
+            // comboBoxHandShake
+            // 
+            this.comboBoxHandShake.FormattingEnabled = true;
+            this.comboBoxHandShake.Location = new System.Drawing.Point(124, 198);
+            this.comboBoxHandShake.Name = "comboBoxHandShake";
+            this.comboBoxHandShake.Size = new System.Drawing.Size(144, 23);
+            this.comboBoxHandShake.TabIndex = 13;
+            // 
+            // labelDebug
+            // 
+            this.labelDebug.AutoSize = true;
+            this.labelDebug.Location = new System.Drawing.Point(12, 612);
+            this.labelDebug.Name = "labelDebug";
+            this.labelDebug.Size = new System.Drawing.Size(79, 15);
+            this.labelDebug.TabIndex = 14;
+            this.labelDebug.Text = "labelDebug";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(22, 317);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(539, 25);
+            this.textBoxInput.TabIndex = 15;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Font = new System.Drawing.Font("Malgun Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBoxOutput.Location = new System.Drawing.Point(23, 362);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOutput.Size = new System.Drawing.Size(537, 202);
+            this.textBoxOutput.TabIndex = 16;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(586, 317);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(104, 25);
+            this.buttonSend.TabIndex = 17;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(586, 362);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(105, 29);
+            this.buttonClear.TabIndex = 18;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // textBoxDataBits
+            // 
+            this.textBoxDataBits.Location = new System.Drawing.Point(124, 129);
+            this.textBoxDataBits.Name = "textBoxDataBits";
+            this.textBoxDataBits.Size = new System.Drawing.Size(144, 25);
+            this.textBoxDataBits.TabIndex = 19;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(331, 25);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 15);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Read Timeout";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(331, 61);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(96, 15);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Write Timeout";
+            // 
+            // textBoxReadTimeout
+            // 
+            this.textBoxReadTimeout.Location = new System.Drawing.Point(434, 22);
+            this.textBoxReadTimeout.Name = "textBoxReadTimeout";
+            this.textBoxReadTimeout.Size = new System.Drawing.Size(144, 25);
+            this.textBoxReadTimeout.TabIndex = 19;
+            // 
+            // textBoxWriteTimeout
+            // 
+            this.textBoxWriteTimeout.Location = new System.Drawing.Point(434, 58);
+            this.textBoxWriteTimeout.Name = "textBoxWriteTimeout";
+            this.textBoxWriteTimeout.Size = new System.Drawing.Size(144, 25);
+            this.textBoxWriteTimeout.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 636);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxWriteTimeout);
+            this.Controls.Add(this.textBoxReadTimeout);
+            this.Controls.Add(this.textBoxDataBits);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.labelDebug);
+            this.Controls.Add(this.comboBoxHandShake);
+            this.Controls.Add(this.comboBoxStopBits);
+            this.Controls.Add(this.comboBoxParity);
+            this.Controls.Add(this.comboBoxBaudRate);
+            this.Controls.Add(this.comboBoxPort);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.ComboBox comboBoxBaudRate;
+        private System.Windows.Forms.ComboBox comboBoxParity;
+        private System.Windows.Forms.ComboBox comboBoxStopBits;
+        private System.Windows.Forms.ComboBox comboBoxHandShake;
+        private System.Windows.Forms.Label labelDebug;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.TextBox textBoxDataBits;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxReadTimeout;
+        private System.Windows.Forms.TextBox textBoxWriteTimeout;
+    }
+}
+
---
+++ SeriaPortAPDUTest/Form1.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+
+namespace SerialAPDUTest
+{
+    public partial class Form1 : Form
+    {
+        static SerialPort _serialPort;
+        static bool _continue;
+        delegate void TextBox_Invoke(string data);
+        delegate void DebugText_Invoke(string message, bool isWriteToLabel);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _serialPort = new SerialPort();
+
+            #region 포트번호 등 설정값 입력
+
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBoxPort.Items.Add(s);
+            }
+
+            if (comboBoxPort.Items.Count > 0)
+                comboBoxPort.SelectedIndex = 0;
+
+            // baudRate 설정 : 4800, 9600, 19200, 38400(default baud rate) and 115200 bps
+            comboBoxBaudRate.Items.Add("4800");
+            comboBoxBaudRate.Items.Add("9600");
+            comboBoxBaudRate.Items.Add("19200");
+            comboBoxBaudRate.Items.Add("38400");
+            comboBoxBaudRate.Items.Add("115200");
+
+            comboBoxBaudRate.SelectedIndex = comboBoxBaudRate.FindString(_serialPort.BaudRate.ToString());
+
+            // parity set
+            foreach (string s in Enum.GetNames(typeof(Parity)))
+            {
+                comboBoxParity.Items.Add(s);
+            }
+
+            comboBoxParity.SelectedIndex = comboBoxParity.FindString(_serialPort.Parity.ToString());
+
+            // data bits
+            textBoxDataBits.Text = _serialPort.DataBits.ToString();
+
+            // stop bits
+            foreach (string s in Enum.GetNames(typeof(StopBits)))
+            {
+                comboBoxStopBits.Items.Add(s);
+            }
+            comboBoxStopBits.SelectedIndex = comboBoxStopBits.FindString(_serialPort.StopBits.ToString());
+
+            // handshake options
+            foreach (string s in Enum.GetNames(typeof(Handshake)))
+            {
+                comboBoxHandShake.Items.Add(s);
+            }
+            comboBoxHandShake.SelectedIndex = comboBoxHandShake.FindString(_serialPort.Handshake.ToString());
+
+            textBoxReadTimeout.Text = "500";
+            textBoxWriteTimeout.Text = "500";
+            #endregion
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            // Allow the user to set the appropriate properties.
+            if(comboBoxPort.SelectedItem == null)
+            {
+                DebugMessage("Port가 선택되지 않았습니다.");
+                MessageBox.Show("Port가 선택되지 않았습니다.");
+                return;
+            }
+
+            _serialPort.PortName = comboBoxPort.SelectedItem.ToString();
+            _serialPort.BaudRate = int.Parse(comboBoxBaudRate.SelectedItem.ToString());
+            _serialPort.Parity = (Parity)Enum.Parse(typeof(Parity), comboBoxParity.SelectedItem.ToString(), true);
+            _serialPort.DataBits = int.Parse(textBoxDataBits.Text);
+            _serialPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), comboBoxStopBits.SelectedItem.ToString(), true);
+            _serialPort.Handshake = (Handshake)Enum.Parse(typeof(Handshake), comboBoxHandShake.SelectedItem.ToString(), true);
+
+            // Set the read/write timeouts
+            _serialPort.ReadTimeout = int.Parse(textBoxReadTimeout.Text);
+            _serialPort.WriteTimeout = int.Parse(textBoxWriteTimeout.Text);
+
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(Serial_DataReceived);
+
+            try
+            {
+                _serialPort.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                DebugMessage(ex.Message);
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+            DebugMessage("Serial 연결에 성공했습니다.");
+
+            _continue = true;
+            buttonConnect.Enabled = false;
+            buttonClose.Enabled = true;
+        }
+
+        private void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort port = (SerialPort)sender;
+            byte[] data = new byte[port.BytesToRead];
+            port.Read(data, 0, data.Length);
+            string message = ByteToHexaString(data);
+            Console.WriteLine(message);
+            WriteDataToOutput(message);
+        }
+
+        public void Read()
+        {
+            while (_continue)
+            {
+                try
+                {
+                    string message = _serialPort.ReadLine();
+                    Console.WriteLine(message);
+                    textBoxOutput.Text += string.Format("{0}\r\n", message);
+                    textBoxOutput.Text += string.Format("{0}\r\n", ByteToHexaString(Encoding.Default.GetBytes(message)));
+                }
+                catch (TimeoutException) { }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+
+        // Display Port values and prompt user to enter a port.
+        public static string SetPortName(string defaultPortName)
+        {
+            string portName;
+
+            Console.WriteLine("Available Ports:");
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            Console.Write("Enter COM port value (Default: {0}): ", defaultPortName);
+            portName = Console.ReadLine();
+
+            if (portName == "" || !(portName.ToLower()).StartsWith("com"))
+            {
+                portName = defaultPortName;
+            }
+            return portName;
+        }
+        // Display BaudRate values and prompt user to enter a value.
+        public static int SetPortBaudRate(int defaultPortBaudRate)
+        {
+            string baudRate;
+
+            Console.Write("Baud Rate(default:{0}): ", defaultPortBaudRate);
+            baudRate = Console.ReadLine();
+
+            if (baudRate == "")
+            {
+                baudRate = defaultPortBaudRate.ToString();
+            }
+
+            return int.Parse(baudRate);
+        }
+
+        // Display PortParity values and prompt user to enter a value.
+        public static Parity SetPortParity(Parity defaultPortParity)
+        {
+            string parity;
+
+            Console.WriteLine("Available Parity options:");
+            foreach (string s in Enum.GetNames(typeof(Parity)))
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            Console.Write("Enter Parity value (Default: {0}):", defaultPortParity.ToString(), true);
+            parity = Console.ReadLine();
+
+            if (parity == "")
+            {
+                parity = defaultPortParity.ToString();
+            }
+
+            return (Parity)Enum.Parse(typeof(Parity), parity, true);
+        }
+        // Display DataBits values and prompt user to enter a value.
+        public static int SetPortDataBits(int defaultPortDataBits)
+        {
+            string dataBits;
+
+            Console.Write("Enter DataBits value (Default: {0}): ", defaultPortDataBits);
+            dataBits = Console.ReadLine();
+
+            if (dataBits == "")
+            {
+                dataBits = defaultPortDataBits.ToString();
+            }
+
+            return int.Parse(dataBits.ToUpperInvariant());
+        }
+
+        // Display StopBits values and prompt user to enter a value.
+        public static StopBits SetPortStopBits(StopBits defaultPortStopBits)
+        {
+            string stopBits;
+
+            Console.WriteLine("Available StopBits options:");
+            foreach (string s in Enum.GetNames(typeof(StopBits)))
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            Console.Write("Enter StopBits value (None is not supported and \n" +
+             "raises an ArgumentOutOfRangeException. \n (Default: {0}):", defaultPortStopBits.ToString());
+            stopBits = Console.ReadLine();
+
+            if (stopBits == "")
+            {
+                stopBits = defaultPortStopBits.ToString();
+            }
+
+            return (StopBits)Enum.Parse(typeof(StopBits), stopBits, true);
+        }
+        public static Handshake SetPortHandshake(Handshake defaultPortHandshake)
+        {
+            string handshake;
+
+            Console.WriteLine("Available Handshake options:");
+            foreach (string s in Enum.GetNames(typeof(Handshake)))
+            {
+                Console.WriteLine("   {0}", s);
+            }
+
+            Console.Write("Enter Handshake value (Default: {0}):", defaultPortHandshake.ToString());
+            handshake = Console.ReadLine();
+
+            if (handshake == "")
+            {
+                handshake = defaultPortHandshake.ToString();
+            }
+
+            return (Handshake)Enum.Parse(typeof(Handshake), handshake, true);
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            _continue = false;
+            _serialPort.Close();
+            buttonConnect.Enabled = true;
+            buttonClose.Enabled = false;
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if(_continue)
+            {
+                string sendText = textBoxInput.Text.Replace(" ", "");
+
+                textBoxOutput.AppendText(string.Format("[{0}] Send : \r\n{1}\r\n", System.DateTime.Now.ToString("HH:mm:ss"), MakeHexaSpace(sendText)));
+                byte[] sendData = HexaStringtoByte(sendText);
+                try
+                {
+                    _serialPort.Write(sendData, 0, sendText.Length / 2);
+                }
+                catch(Exception ex)
+                {
+                    DebugMessage(ex.Message);
+                    Console.WriteLine(ex);
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+
+                textBoxOutput.Clear();
+            }
+        }
+
+        private string MakeHexaSpace(string text)
+        {
+            string newText = string.Empty;
+
+            int counter = 1;
+            foreach(char character in text)
+            {
+                newText += character;
+                if (counter % 2 == 0) newText += " ";
+                if (counter % 20 == 0)
+                    newText += "\r\n";
+                //    newText += "\t" + Encoding.Unicode.GetString(HexaStringtoByte(text.Substring(counter - 20, 20))) + "\r\n";
+                counter++;
+            }
+            return newText.ToUpper();
+        }
+
+        public string StringtoHexaString(string data)
+        {
+            string resultString = string.Empty;
+
+            byte[] stringArray = Encoding.Default.GetBytes(data);
+
+            foreach (byte character in stringArray)
+                resultString += string.Format("{0:X2}", character);
+
+            return resultString;
+        }
+
+        public byte[] HexaStringtoByte(string data)
+        {
+            byte[] resultArray = new byte[data.Length / 2];
+
+            for (int i = 0; i < resultArray.Length; i++)
+                resultArray[i] = Convert.ToByte(data.Substring(i * 2, 2), 16);
+
+            return resultArray;
+        }
+
+        public string ByteToHexaString(byte[] data)
+        {
+            string resultString = string.Empty;
+
+            resultString = string.Concat(Array.ConvertAll(data, byt => byt.ToString("X2")));
+
+            return resultString;
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxOutput.Clear();
+        }
+
+        private void WriteDataToOutput(string message)
+        {
+            if (textBoxOutput.InvokeRequired)
+            {
+                TextBox_Invoke invoke = new TextBox_Invoke(WriteDataToOutput);
+                textBoxOutput.Invoke(invoke, message);
+            }
+            else
+            {
+                textBoxOutput.AppendText(string.Format("[{0}] Recv : \r\n{1}\r\n", System.DateTime.Now.ToString("HH:mm:ss"), MakeHexaSpace(message)));
+            }
+        }
+
+        private void DebugMessage(string message, bool isWriteToLabel = true)
+        {
+            if (labelDebug.InvokeRequired)
+            {
+                DebugText_Invoke invoke = new DebugText_Invoke(DebugMessage);
+                labelDebug.Invoke(invoke, message, isWriteToLabel);
+            }
+            else
+            {
+                string logMessage = string.Format("[{0}] {1}", System.DateTime.Now.ToString("HH:mm:ss"), message);
+                Console.WriteLine(logMessage);
+                if(isWriteToLabel == true)
+                    labelDebug.Text = logMessage;
+            }
+        }
+    }
+}
